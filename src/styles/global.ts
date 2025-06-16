27e098b9d1eb66e5dcb25eb38cd5b261f3de6a76
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-06-16 17:01:31 -0300

    style: add global styles and default theme

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+     * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: ${props => props.theme.colors.background};
+        color: ${props => props.theme.colors['base-text']};
+        -webkit-font-smoothing: antialiased;
+    }
+    
+    body, input, textarea, button {
+        font-family: ${props => props.theme.fonts.family.text}, sans-serif;
+        font-weight: ${props => props.theme.fonts.weight.regular};
+        font-size: ${props => props.theme.fonts.size['text-m']};
+    }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,55 @@
+export const defaultTheme = {
+    colors: {
+        'white': '#FFFFFF',
+
+        'background': '#FAFAFA',
+        'base-card': '#F3F2F2',
+        'base-input': '#EDEDED',
+        'base-button': '#E6E5E5',
+        'base-hover': '#D7D5D5',
+        'base-label': '#8D8686',
+        'base-text': '#574F4D',
+        'base-subtitle': '#403937',
+        'base-title': '#272221',
+
+        'yellow-dark': '#C47F17',
+        'yellow': '#DBAC2C',
+        'yellow-light': '#F1E9C9',
+
+        'purple-dark': '#4B2995',
+        'purple': '#8047F8',
+        'purple-light': '#EBE5F9'
+    },
+
+    fonts: {
+        family: {
+            title: "'Baloo 2', cursive",
+            text: "'Roboto', sans-serif"
+        },
+        size: {
+            'title-xl': '3rem',     // 48px
+            'title-l': '2rem',      // 32px
+            'title-m': '1.5rem',    // 24px
+            'title-s': '1.25rem',   // 20px
+            'title-xs': '1.125rem', // 18px
+
+            'text-l': '1.25rem',    // 20px
+            'text-m': '1rem',       // 16px
+            'text-s': '0.875rem',   // 14px
+            'text-xs': '0.75rem',   // 12px
+
+            'tag': '0.625rem',      // 10px
+            'button-g': '0.875rem', // 14px
+            'button-m': '0.75rem'   // 12px
+        },
+        weight: {
+            'regular': 400,
+            'bold': 700,
+            'extra-bold': 800
+        },
+        lineHeight: {
+            'base': 1.3,
+            'button': 1.6,
+        }
+    }
+}
