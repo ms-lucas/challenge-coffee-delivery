c8f1bbd19101ed29ed1af613ac67a46f16263bb8
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-06-17 10:21:50 -0300

    feat: configure application routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from "styled-components";
+
+import { AppRoutes } from "./routes";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from '../pages/Home'
+import { Checkout } from '../pages/Checkout'
+import { Success } from '../pages/Success'
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/success" element={<Success />} />
+        </Routes>
+    )
+}
